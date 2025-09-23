d2fd19c83ff11f9fff0e79d9e80c84fd71d47fd1
Nad Alaba <37968805+NadAlaba@users.noreply.github.com>
2025-09-23 21:45:37 +0300

    add author 'Nad Alaba'

--- scripts/make-package.sh
+++ scripts/make-package.sh
@@ -8,6 +8,6 @@ VERSION="$(python3 ${SCRIPT_DIR}/get-version.py ${REPO_DIR}/fonts/ttf/Vazirharf-
 # Make the release package file (zip)
 RELEASEFILEPATH="vazirharf-v$VERSION.zip"
 rm -f "$RELEASEFILEPATH" || exit 1
-zip -r "$RELEASEFILEPATH" -q -MM fonts misc Round-Dots Vazirharf-font-face.css Vazirharf-Variable-font-face.css CHANGELOG.md README.md OFL.txt AUTHORS.txt راهنما.txt || exit 1
+zip -r "$RELEASEFILEPATH" -q -MM fonts misc Round-Dots Vazirharf-font-face.css Vazirharf-Variable-font-face.css CHANGELOG.md README.md OFL.txt AUTHORS.txt DOCS.txt || exit 1
 
 echo "Created $RELEASEFILEPATH"
