dd0b525ccab9fecd1b273b58d79dfa47e0d81b38
Nad Alaba <37968805+NadAlaba@users.noreply.github.com>
2025-10-03 18:16:56 +0300

    apply new-link codemod from next

--- website/src/Link.tsx
+++ website/src/Link.tsx
@@ -43,7 +43,7 @@ export const NextLinkComposed = React.forwardRef<
       shallow={shallow}
       passHref
       locale={locale}
-    >
+      legacyBehavior>
       <Anchor ref={ref} {...other} />
     </NextLink>
   );
