cc463c74513695950e3b5e9704036b11fd9dfe8b
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-02-10 20:54:40 +0300

    simplify constants

--- website/src/lib/constants.ts
+++ website/src/lib/constants.ts
@@ -5,17 +5,16 @@ export const BASE_PATH =
 export const DOWNLOAD_BASE_URL =
   "https://github.com/nadalaba/vazirharf/releases/download";
 
-export const TAG_NAME = process.env.NEXT_PUBLIC_TAG_NAME
-  ? process.env.NEXT_PUBLIC_TAG_NAME
-  : process.env.NEXT_PUBLIC_LAST_TAG_NAME
-  ? process.env.NEXT_PUBLIC_LAST_TAG_NAME
-  : "v0.000";
+export const TAG_NAME =
+  process.env.NEXT_PUBLIC_TAG_NAME ||
+  process.env.NEXT_PUBLIC_LAST_TAG_NAME ||
+  "v0.000";
 
 export const DOWNLOAD_URL = `${DOWNLOAD_BASE_URL}/${TAG_NAME}/vazirharf-${TAG_NAME}.zip`;
 
-export const VAZIRHARF_CDN_URL = process.env.NEXT_PUBLIC_CDN_URL
-  ? process.env.NEXT_PUBLIC_CDN_URL
-  : `https://cdn.jsdelivr.net/gh/nadalaba/vazirharf@${TAG_NAME}`;
+export const VAZIRHARF_CDN_URL =
+  process.env.NEXT_PUBLIC_CDN_URL ||
+  `https://cdn.jsdelivr.net/gh/nadalaba/vazirharf@${TAG_NAME}`;
 
 export const VAZIRHARF_CSS_URL = `${VAZIRHARF_CDN_URL}/Vazirharf-font-face.css`;
 export const VAZIRHARF_RD_CSS_URL = `${VAZIRHARF_CDN_URL}/Round-Dots/Vazirharf-RD-font-face.css`;
