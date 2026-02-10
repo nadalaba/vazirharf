393508bc82d0f798701b74a617e5f8f0c92e7449
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-02-10 14:36:53 +0300

    format SelectButton.tsx

--- website/src/components/SelectButton.tsx
+++ website/src/components/SelectButton.tsx
@@ -1,47 +1,42 @@
 "use client";
 
 import Button from "@mui/material/Button";
-import {
-  useColorScheme,
-  styled,
-  Theme
-} from "@mui/material/styles";
+import { useColorScheme, styled, Theme } from "@mui/material/styles";
 
-export const SelectButton = styled(Button)(
-  ({
-    color,
-    variant,
-    selected,
-    theme,
-  }: {
-    color: string;
-    variant: string;
-    selected: boolean;
-    theme?: Theme;
-  }) => {
-    const { mode, systemMode } = useColorScheme();
-    let backgroundColor = undefined;
-    if (selected && theme) {
-      if (color === "primary" || color === "secondary") {
-        if (variant === "text" || variant === "outlined") {
-          backgroundColor =
-            mode !== "dark" && !(mode === "system" && systemMode === "dark")
-              ? `color-mix(in srgb, ${theme.vars?.palette[color].main} 20%, white)` // equivalent to lighten(theme.palette[color].main, 0.8)
-              : `color-mix(in srgb, ${theme.vars?.palette[color].main} 40%, black)`; // equivalent to darken(theme.palette[color].main, 0.6);
-        } else {
-          backgroundColor =
-            mode !== "dark" && !(mode === "system" && systemMode === "dark")
-              ? theme.vars?.palette[color].dark
-              : `color-mix(in srgb, ${theme.vars?.palette[color].main} 40%, white)`; // equivalent to lighten(theme.palette[color].main, 0.6)
-        }
+export const SelectButton = styled(Button)(({
+  color,
+  variant,
+  selected,
+  theme,
+}: {
+  color: string;
+  variant: string;
+  selected: boolean;
+  theme?: Theme;
+}) => {
+  const { mode, systemMode } = useColorScheme();
+  const colorMode = mode === "system" ? systemMode : mode;
+  let backgroundColor = undefined;
+  if (selected && theme) {
+    if (color === "primary" || color === "secondary") {
+      if (variant === "text" || variant === "outlined") {
+        backgroundColor =
+          colorMode !== "dark"
+            ? `color-mix(in srgb, ${theme.vars?.palette[color].main} 20%, white)` // equivalent to lighten(theme.palette[color].main, 0.8)
+            : `color-mix(in srgb, ${theme.vars?.palette[color].main} 40%, black)`; // equivalent to darken(theme.palette[color].main, 0.6);
       } else {
-        backgroundColor = theme.vars?.palette.action.selected;
+        backgroundColor =
+          colorMode !== "dark"
+            ? theme.vars?.palette[color].dark
+            : `color-mix(in srgb, ${theme.vars?.palette[color].main} 40%, white)`; // equivalent to lighten(theme.palette[color].main, 0.6)
       }
+    } else {
+      backgroundColor = theme.vars?.palette.action.selected;
     }
-    return {
-      backgroundColor,
-      ":focus": { backgroundColor },
-      ":hover": { backgroundColor },
-    };
   }
-);
+  return {
+    backgroundColor,
+    ":focus": { backgroundColor },
+    ":hover": { backgroundColor },
+  };
+});
