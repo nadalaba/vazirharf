5bed7cae41c84a6c22ff230101d90b865d6997ed
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-02-27 23:22:03 +0300

    fix donations list reponsivity

--- website/src/components/DonationList.tsx
+++ website/src/components/DonationList.tsx
@@ -10,7 +10,6 @@ import {useLocale} from 'next-intl';
 import { languages } from "@/i18n/settings";
 
 import { convertNumberToPersian, formatNumber } from "@/lib/utils";
-import { useWindowSize } from "@/lib/windowSize";
 import { Donation } from "@/types";
 
 type Props = {
@@ -20,26 +19,28 @@ type Props = {
 export const DonationList = (props: Props) => {
   const theme = useTheme();
   const locale = useLocale();
-  useWindowSize(); // rerender on resize
   const { donations } = props;
-
-  const listRef = useRef<HTMLDivElement | null>(null);
+  
   const [viewWidth, setViewWidth] = useState(0);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (listRef.current) {
-      setViewWidth(listRef.current.scrollWidth);
-    }
-  }, []);
-  const itemWidth = 65;
-  const itemHeight = 140;
-  const itemsPerRow = Math.floor(viewWidth / itemWidth);
+    const resizeObserver = new ResizeObserver((entries) => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry/contentBoxSize
+      setViewWidth(entries[0].contentBoxSize[0].inlineSize);
+    });
+    if (containerRef?.current) resizeObserver.observe(containerRef.current);
+  }, [containerRef]);
+
+  const itemWidth = 90;
+  const itemHeight = 100;
+  const itemsPerRow = Math.floor(viewWidth / itemWidth) || 1;
   const rowCount = Math.ceil(donations.length / itemsPerRow);
-  const listHeight = 300;
+  const listHeight = Math.min(rowCount, 2) * itemHeight + 10;
 
   return (
     <Box
-      ref={listRef}
+      ref={containerRef}
       sx={{
         display: "flex",
         justifyContent: "center",
@@ -53,92 +54,95 @@ export const DonationList = (props: Props) => {
         overflow: "hidden",
       }}
     >
-      <List
-        rowCount={rowCount}
-        rowHeight={itemHeight}
-        dir={languages[locale].direction}
-        rowProps={{}}
-        overscanCount={4}
-        rowComponent={({ index, style }: RowComponentProps) => {
-          const items = donations.slice(
-            index * itemsPerRow,
-            index * itemsPerRow + itemsPerRow
-          );
-          return (
-            <Box style={style}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  width: "100%",
-                }}
-              >
-                {items.map((d) => (
-                  <div
-                    key={d.name + d.date}
-                    style={{
-                      width: `${itemWidth}px`,
-                      fontSize: "0.7rem",
-                      lineHeight: 1.3,
-                      float: "right",
-                      overflow: "hidden",
-                      paddingTop: 8,
-                    }}
-                  >
-                    <a
-                      href={d.web ? d.web : undefined}
-                      title={`${d.date}\n${d.name}`}
+      {viewWidth > 0 && (
+        <List
+          rowCount={rowCount}
+          rowHeight={itemHeight}
+          dir={languages[locale].direction}
+          rowProps={{}}
+          overscanCount={4}
+          rowComponent={({ index, style }: RowComponentProps) => {
+            const items = donations.slice(
+              index * itemsPerRow,
+              index * itemsPerRow + itemsPerRow
+            );
+            return (
+              <Box style={style}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                  }}
+                >
+                  {items.map((d, i) => (
+                    <div
+                      key={i}
                       style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "start",
-                        alignItems: "center",
+                        width: `${itemWidth}px`,
+                        fontSize: "0.7rem",
+                        lineHeight: 1.3,
+                        float: "right",
+                        overflow: "hidden",
                         paddingTop: 8,
-                        height: `${itemHeight}px`,
-                        color: !d.web ? theme.vars?.palette.text.primary : undefined,
                       }}
-                      target="__blank"
                     >
-                      <Image
-                        src={
-                          d.img ??
-                          `https://s.gravatar.com/avatar/${d.date.replace(
-                            /(\s)|(:)|(\/)/g,
-                            ""
-                          )}?noemail&s=50&d=wavatar`
-                        }
-                        alt={d.name}
-                        width={50}
-                        height={50}
-                        style={{ borderRadius: "50%" }}
-                      />
-                      <div
+                      <a
+                        href={d.web ? d.web : undefined}
+                        title={`${d.date}\n${d.name}`}
                         style={{
-                          paddingTop: "4px",
-                          overflow: "hidden",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "start",
+                          alignItems: "center",
+                          paddingTop: 8,
+                          height: `${itemHeight}px`,
+                          color: !d.web ? theme.vars?.palette.text.primary : undefined,
                         }}
+                        target="__blank"
+                        rel="noreferrer"
                       >
-                        {d.name}
-                      </div>
-                      <div
-                        style={{
-                          fontSize: "0.6rem",
-                          color: theme.vars?.palette.text.primary,
-                          opacity: 0.5,
-                        }}
-                      >
-                        {locale === "fa"
-                          ? convertNumberToPersian(formatNumber(d.amount, "٬"))
-                          : formatNumber(d.amount, ",")}
-                      </div>
-                    </a>
-                  </div>
-                ))}
+                        <Image
+                          src={
+                            d.img ??
+                            `https://s.gravatar.com/avatar/${d.date.replace(
+                              /(\s)|(:)|(\/)/g,
+                              ""
+                            )}?noemail&s=50&d=wavatar`
+                          }
+                          alt={d.name}
+                          width={50}
+                          height={50}
+                          style={{ borderRadius: "50%" }}
+                        />
+                        <div
+                          style={{
+                            paddingTop: "4px",
+                            overflow: "hidden"
+                          }}
+                        >
+                          {d.name}
+                        </div>
+                        <div
+                          style={{
+                            fontSize: "0.6rem",
+                            color: theme.vars?.palette.text.primary,
+                            opacity: 0.5,
+                          }}
+                        >
+                          {locale === "fa"
+                            ? convertNumberToPersian(formatNumber(d.amount, "٬"))
+                            : formatNumber(d.amount, ",")}
+                        </div>
+                      </a>
+                    </div>
+                  ))}
+                </Box>
               </Box>
-            </Box>
-          );
-        }}
-      />
+            );
+          }}
+        />
+      )}
     </Box>
   );
 };
