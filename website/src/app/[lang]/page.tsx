f98de2872900d00631a574f06c25c94c0fdf9de9
Nad Alaba <37968805+nadalaba@users.noreply.github.com>
2026-03-05 02:31:05 +0300

    minimize flicker in website's donations container on refresh and language change (#23)
    * fetch donations on server instead of client component
    
    * fix donations container's height to 2 rows

--- website/src/app/[lang]/page.tsx
+++ website/src/app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 import { setRequestLocale } from "next-intl/server";
 import { Layout } from "@/components/Layout";
-import { SITE_NAME, SITE_BASE_PATH } from "@/lib/constants";
+import { SITE_NAME, SITE_BASE_PATH, BASE_PATH } from "@/lib/constants";
 import { defaultLang } from "@/i18n/settings";
 import { IndexLayout } from "./client";
 
@@ -15,9 +15,11 @@ export default async function Index(props: PageProps<"/[lang]">) {
   const { lang } = await props.params;
   // Enable static rendering
   setRequestLocale(lang);
+  const response = await fetch(`${SITE_BASE_PATH}/donations.json`);
+  const donations = await response.json();
   return (
     <Layout stableScrollBar>
-      <IndexLayout />
+      <IndexLayout donations={donations}/>
     </Layout>
   );
 };
